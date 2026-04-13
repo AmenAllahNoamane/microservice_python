9236dce2f8b3cee97baac950e6655f0a089cc67c
amennoamen <amenallah.noamane@fsb.ucar.tn>
2026-04-13 18:06:36 +0100

    13/04

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ async def upload_file(file: UploadFile = File(...)):
 
         # LLM
         llm = classify_document(ocr["clean_text"])
+        print(llm)
         if not llm.get("succes"):
             return JSONResponse(status_code=500, content={"succes": False, "erreur": llm.get("erreur")})
 
